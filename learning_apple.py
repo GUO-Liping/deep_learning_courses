1ff48793027228c4662f699bfff786e49004ee5c
Liping0_0 <Liping0_0@my.swjtu.edu.cn>
2023-08-16 10:05:28 +0800

    Update learning_apple.py

--- learning_apple.py
+++ learning_apple.py
@@ -6,6 +6,7 @@ import os
 '''
 采用神经网络识别苹果，训练集由分辨率均为196pixel×196pixel的10张苹果图片组成
 程序测试表明：归一化、赋初值、学习率的设定是影响收敛与否的关键因素
+正反样本的选择会提高预测准确率
 '''
 
 # 加载图片，生成训练集或测试集，并对数据进行预处理
@@ -35,25 +36,25 @@ def load_image_data(dir_data):
 	return data_set_x
 
 
-# 逻辑回归函数: sigmoid
+# Graded function: sigmoid
 
 def sigmoid(z):
     """
-    Compute the sigmoid of z
+    This function compute the sigmoid of z
 
-    Arguments:
-    z -- A scalar or numpy array of any size.
+    Argument:
+    z -- a scalar or numpy array
 
-    Return:
+    Return：
     s -- sigmoid(z)
     """
 
-    ### START CODE HERE ### (≈ 1 line of code)
     s = 1 / (1 + np.exp(-z))
-    ### END CODE HERE ###
     
     return s
 
+
+# Graded function: Initialization
 
 def initialize_with_zeros(dim):
     """
@@ -67,17 +68,16 @@ def initialize_with_zeros(dim):
     b -- initialized scalar (corresponds to the bias)
     """
     
-    ### START CODE HERE ### (≈ 1 line of code)
     w = np.zeros((dim, 1))
     b = 0
-    ### END CODE HERE ###
 
     assert(w.shape == (dim, 1))
     assert(isinstance(b, float) or isinstance(b, int))
     
     return w, b
 
-# GRADED FUNCTION: propagate
+
+# Graded function: propagate
 
 def propagate(w, b, X, Y):
     """
@@ -87,34 +87,27 @@ def propagate(w, b, X, Y):
     w -- weights, a numpy array of size (num_px * num_px * 3, 1)
     b -- bias, a scalar
     X -- data of size (num_px * num_px * 3, number of examples)
-    Y -- true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
+    Y -- true "label" vector (containing 0 if non-apple, 1 if apple) of size (1, number of examples)
 
     Return:
     cost -- negative log-likelihood cost for logistic regression
     dw -- gradient of the loss with respect to w, thus same shape as w
     db -- gradient of the loss with respect to b, thus same shape as b
-    
-    Tips:
-    - Write your code step by step for the propagation. np.log(), np.dot()
     """
     
     m = X.shape[1]
     
     # FORWARD PROPAGATION (FROM X TO COST)
-    ### START CODE HERE ### (≈ 2 lines of code)
     A = sigmoid(np.dot(w.T, X) + b)            # compute activation
     cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))         # compute cost
-    ### END CODE HERE ###
  
     # BACKWARD PROPAGATION (TO FIND GRAD)
-    ### START CODE HERE ### (≈ 2 lines of code)
     dw = 1 / m * np.dot(X, (A - Y).T)
     db = 1 / m * np.sum(A - Y)
-    ### END CODE HERE ###
 
     assert(dw.shape == w.shape)
     assert(db.dtype == float)
-    cost = np.squeeze(cost)  # 删除数组中的单维度条目
+    cost = np.squeeze(cost)  # remove axes of length one from a numpy array
     assert(cost.shape == ())
     
     grads = {"dw": dw,
@@ -123,7 +116,7 @@ def propagate(w, b, X, Y):
     return grads, cost
 
 
-# GRADED FUNCTION: optimize
+# Graded function: optimize
 
 def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
     """
@@ -142,11 +135,6 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
     params -- dictionary containing the weights w and bias b
     grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
     costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
-    
-    Tips:
-    You basically need to write down two steps and iterate through them:
-        1) Calculate the cost and the gradient for the current parameters. Use propagate().
-        2) Update the parameters using gradient descent rule for w and b.
     """
     
     costs = []
@@ -154,21 +142,18 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
     for i in range(num_iterations):
         
         
-        # Cost and gradient calculation (≈ 1-4 lines of code)
-        ### START CODE HERE ### 
+        # Cost and gradient calculation
         grads, cost = propagate(w, b, X, Y)
-        ### END CODE HERE ###
         
         # Retrieve derivatives from grads
         dw = grads["dw"]
         db = grads["db"]
         
-        # update rule (≈ 2 lines of code)
-        ### START CODE HERE ###
+        # update rule
         w = w - learning_rate * dw
         b = b - learning_rate * db
-        ### END CODE HERE ###
         print('loop = ', i,'dw=',dw[0], 'db=',db, 'cost =', cost )
+
         # Record the costs
         if i % 100 == 0:
             costs.append(cost)
@@ -186,7 +171,7 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
     return params, grads, costs
 
 
-# GRADED FUNCTION: predict
+# Graded function: predict
 
 def predict(w, b, X):
     '''
@@ -206,19 +191,15 @@ def predict(w, b, X):
     w = w.reshape(X.shape[0], 1)
     
     # Compute vector "A" predicting the probabilities of a cat being present in the picture
-    ### START CODE HERE ### (≈ 1 line of code)
     A = sigmoid(np.dot(w.T, X) + b)
-    ### END CODE HERE ###
 
     for i in range(A.shape[1]):
         
         # Convert probabilities A[0,i] to actual predictions p[0,i]
-        ### START CODE HERE ### (≈ 4 lines of code)
         if A[0, i] <= 0.5:
             Y_prediction[0, i] = 0
         else:
             Y_prediction[0, i] = 1
-        ### END CODE HERE ###
     
     assert(Y_prediction.shape == (1, m))
     
@@ -243,31 +224,26 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations = 100, learning_rate 
     Returns:
     d -- dictionary containing information about the model.
     """
-    
-    ### START CODE HERE ###
-    
-    # initialize parameters with zeros (≈ 1 line of code)
+      
+    # initialize parameters with zeros
     w, b = initialize_with_zeros(X_train.shape[0])
 
-    # Gradient descent (≈ 1 line of code)
+    # Gradient descent
     parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
     
     # Retrieve parameters w and b from dictionary "parameters"
     w = parameters["w"]
     b = parameters["b"]
     
-    # Predict test/train set examples (≈ 2 lines of code)
+    # Predict test/train set examples
     Y_prediction_train = predict(w, b, X_train)
     Y_prediction_test = predict(w, b, X_test)
     print('Y_prediction_train=',Y_prediction_train, 'Y_prediction_test=',Y_prediction_test)
-
-    ### END CODE HERE ###
 
     # Print train/test Errors
     print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
     print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
 
-    
     d = {"costs": costs,
          "Y_prediction_test": Y_prediction_test, 
          "Y_prediction_train" : Y_prediction_train, 
@@ -282,9 +258,18 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations = 100, learning_rate 
 
 
 if __name__ == '__main__':
+    # Get the data set for training and testing
     X_train = load_image_data("apple_train")
     Y_train = np.array([0,1,0,1,0,1,1,1,1])
     X_test = load_image_data("apple_test")
     Y_test = np.array([1,1,0,0,0,1,1,1,1])
+
+    # Get the model parameters
     print('X_train=',X_train.shape, '\n X_test=', X_test.shape)
-    model(X_train, Y_train, X_test, Y_test, num_iterations = 1000, learning_rate = 0.003, print_cost = False)
+    apple_model = model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.003, print_cost = False)
+    costs_model = apple_model["costs"]
+    num_iterations =  apple_model["num_iterations"]
+
+    # plot the costs with respect to num_iteration
+    plt.plot(np.arange(num_iterations/100),costs_model)
+    plt.show()
